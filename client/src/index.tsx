ddad9130126b453776a3e4d6bee4b3eab46695b5
Elise Verhoeye <eliseverhoeye@hotmail.com>
2023-03-18 12:34:04 +0000

    chore: introduced browser router and login component

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,14 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from "./Components/User/Login";
+
+// type createBrowserRouter(
+//   routes: RouteObject[],
+//   opts?: {
+//     basename?: string;
+//     window?: Window;
+//   }
+// ): RemixRouter;
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+  },
+]);
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <RouterProvider router={router} />
     <App />
   </React.StrictMode>
 );
