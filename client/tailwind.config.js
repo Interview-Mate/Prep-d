af34e7a159dc581f24c41e1fc0beb8664722edd2
Elise Verhoeye <eliseverhoeye@hotmail.com>
2023-03-18 17:04:56 +0000

    style: tried overwriting tailwind

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,6 +3,21 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    theme: {
+      screens: {
+        sm: "480px",
+        md: "768px",
+        lg: "976px",
+        xl: "1440px",
+      },
+      // colors: {
+      //   eerieblack: rgba(38, 38, 38, 1),
+      //   darkcyan: rgba(84, 134, 135, 1),
+      //   africanviolet: rgba(201, 139, 185, 1),
+      //   seasalt: rgba(248, 247, 249, 1),
+      //   white: rgba(252, 252, 252, 1),
+      // },
+    },
   },
   plugins: [require("@tailwindcss/forms")],
 };
