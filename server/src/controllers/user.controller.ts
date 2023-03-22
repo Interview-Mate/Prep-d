e22928e48e9718de1297aae23d051f269714cf5d
Elise Verhoeye <eliseverhoeye@hotmail.com>
2023-03-22 18:57:19 +0000

    fix: minor cleanup

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -1,4 +1,4 @@
-import User from '../models/user';
+import User from "../models/user";
 import { Request, Response } from "express";
 
 //DB TEST:
@@ -8,92 +8,58 @@ import { Request, Response } from "express";
 //   "username": "jd",
 //   "email": "jd@asd.com",
 //   "password": "123123",
-//   "level": "1"
+//   "level": "intermediate"
 // }));
 
 exports.getAllUsers = async (req: Request, res: Response) => {
   try {
     let allUsers = await User.find();
-    res
-      .status(200)
-      .json(allUsers);
+    res.status(200).json(allUsers);
   } catch (err) {
     console.log(err);
-    res
-      .json(err)
-      .status(500);
+    res.json(err).status(500);
   }
 };
 
-//TODO: login - AuthO;
-exports.createUser = async (req: Request, res: Response)=> {
+exports.createUser = async (req: Request, res: Response) => {
   try {
-    let existingUsername = await User.find({username : req.body.username})
-    if(existingUsername.length > 0){
-      throw new Error('A user with this username already exists')
-    }else{
-      let user = await User.create(
-        {
-          name:  req.body.name,
-          surname: req.body.surname,
-          username: req.body.username,
-          email: req.body.email,
-          password: req.body.password,
-          level: req.body.level,
-        });
-      console.log('User created');
-      res
-        .status(201)
-        .json(user);
-    }
+    let user = await User.create(req.body);
+    console.log("User created", user);
+    res.status(201).json(user);
   } catch (err: any) {
-    res
-      .status(403)
-      .json(err.message)
+    res.status(403).json(err.message);
   }
 };
 
 exports.getUser = async (req: Request, res: Response) => {
   try {
-    let id = req.params.id;
-    let result = await User.findById(id);
-    if (!result) {
+    const email = req.params.email;
+    const existingUser = await User.find({ email: email });
+    if (!existingUser) {
       throw new Error("User not found");
     }
-     res
-      .json(result)
-      .status(200);
+    res.json(existingUser).status(200);
   } catch (err: any) {
-     res
-      .status(500)
-      .json(err.message);
+    res.status(500).json(err.message);
   }
 };
 
-exports.editUser =async(req: Request, res: Response) => {
+exports.editUser = async (req: Request, res: Response) => {
   try {
     let id = req.params.id;
     req.body = await User.findByIdAndUpdate(id, req.body);
-    res
-      .status(200)
-      .json(`User with id:${id} was successfully updated.`);
+    res.status(200).json(`User with id:${id} was successfully updated.`);
   } catch (err: any) {
-    res
-      .status(500)
-      .json(err.message);
+    res.status(500).json(err.message);
   }
 };
 
-exports.deleteUser =async (req: Request, res: Response) => {
+exports.deleteUser = async (req: Request, res: Response) => {
   try {
     let id = req.params.id;
-    req.body = await User.deleteOne({ _id: id })
-    res
-      .status(200)
-      .json(`User with id:${id} was successfully deleted.`);
+    req.body = await User.deleteOne({ _id: id });
+    res.status(200).json(`User with id:${id} was successfully deleted.`);
   } catch (err: any) {
-    res
-      .status(500)
-      .json(err.message);
+    res.status(500).json(err.message);
   }
-}
+};
