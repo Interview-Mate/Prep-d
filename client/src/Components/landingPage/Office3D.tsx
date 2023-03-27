5f5dc91dcfe2df44f5b5b2f8ca938cb29242ae45
Sarah Lash <sarah.lash92@gmail.com>
2023-03-27 17:01:29 +0200

    [FE][LandingPage][UI]
    - 3D room tweeks animation on scroll complete

--- client/src/Components/landingPage/Office3D.tsx
+++ client/src/Components/landingPage/Office3D.tsx
@@ -5,10 +5,10 @@ import { useFrame } from '@react-three/fiber';
 
 export function Office3D(): JSX.Element {
   const { nodes, materials } = useGLTF('lib/landingPage/OfficeRoom.gltf') as any; 
-  // TODO: fix the "any" later
-  const ref = useRef();
+
+  
+
   const tl = useRef<gsap.core.Timeline>();
-  const libraryRef = useRef<any>() 
   const BoxRef = useRef<any>() 
   const FurnRef = useRef<any>() 
   const CompRef = useRef<any>() 
@@ -23,74 +23,23 @@ export function Office3D(): JSX.Element {
 
   useLayoutEffect(() => {
 
+
     tl.current = gsap.timeline();
 
 
-    // // library animation 
-    // tl.current.from(
-
-    //   libraryRef.current.position,
-    //   {
-    //     duration: 0.5,
-    //     z: -500,
-    //     ease: 'power2.out',
-    //   },
-    //   0.5
-    // );
-
-    // tl.current.from(
-
-    //   libraryRef?.current.rotation,
-    //   {
-    //     duration: 0.5,
-    //     z: -Math.PI,
-    //   },
-    //   0
-    // );
 
     // // Box animation 
-    // tl.current.from(
-
-    //   BoxRef.current.position,
-    //   {
-    //     duration: 0.5,
-    //     z: -500,
-    //     ease: 'power2.out',
-    //   },
-    //   0.5
-    // );
-
-    // tl.current.from(
-
-    //   BoxRef?.current.rotation,
-    //   {
-    //     duration: 0.5,
-    //     z: -Math.PI,
-    //   },
-    //   0
-    // );
-
-    // // Furniture animation 
     tl.current.from(
 
-      FurnRef.current.position,
+      BoxRef.current.position,
       {
         duration: 0.5,
-        z: -500,
+        x: -500,
         ease: 'power2.out',
       },
       0.5
     );
 
-    // tl.current.from(
-
-    //   FurnRef?.current.rotation,
-    //   {
-    //     duration: 0.5,
-    //     z: -Math.PI,
-    //   },
-    //   0
-    // );
 
     // Computer animation 
     tl.current.from(
@@ -104,27 +53,9 @@ export function Office3D(): JSX.Element {
       0
     );
 
-    // tl.current.from(
 
-    //   CompRef?.current.rotation,
-    //   {
-    //     duration: 0.5,
-    //     z: -Math.PI,
-    //   },
-    //   0
-    // );
+    // Room animation 
 
-    // Computer animation 
-    // tl.current.from(
-
-    //   RoomRef.current.position,
-    //   {
-    //     duration: 0.5,
-    //     x: 500,
-    //     ease: 'power2.out',
-    //   },
-    //   0.5
-    // );
 
     tl.current.from(
 
@@ -147,10 +78,9 @@ export function Office3D(): JSX.Element {
         <mesh geometry={nodes.Office2_base.geometry} material={materials['LP_Rooms.001']} />
         <mesh geometry={nodes.Office2_parquet1.geometry} material={materials['LP_Rooms.001']} />
         <mesh geometry={nodes.Office2_parquet2.geometry} material={materials['LP_Rooms.001']} />
-        
-
+       
         <group position= {[0, 0, 0]} rotation={[0, 0, 0]} >
-          <group ref = {libraryRef}>
+          <group ref = {BoxRef}>
             <mesh geometry={nodes.Office2_magazines1.geometry} material={materials['LP_Rooms.001']} />
             <mesh geometry={nodes.Office2_magazines2.geometry} material={materials['LP_Rooms.001']} />
             <mesh geometry={nodes.Office2_magazines3.geometry} material={materials['LP_Rooms.001']} />
@@ -174,11 +104,6 @@ export function Office3D(): JSX.Element {
             <mesh geometry={nodes.Office2_Books7.geometry} material={materials['LP_Rooms.001']} />
             <mesh geometry={nodes.Office2_Books8.geometry} material={materials['LP_Rooms.001']} />
             <mesh geometry={nodes.Office2_Books9.geometry} material={materials['LP_Rooms.001']} />
-          </group>
-        </group>
-       
-        <group position= {[0, 0, 0]} rotation={[0, 0, 0]} >
-          <group ref = {BoxRef}>
             <mesh geometry={nodes.Office2_vial_1.geometry} material={materials['LP_Rooms.001']} />
             <mesh geometry={nodes.Office2_vial_2.geometry} material={materials['LP_Rooms.001']} />
             <mesh geometry={nodes.Office2_box_1.geometry} material={materials['LP_Rooms.001']} />
@@ -188,17 +113,17 @@ export function Office3D(): JSX.Element {
             <mesh geometry={nodes.Office2_box_5.geometry} material={materials['LP_Rooms.001']} />
             <mesh geometry={nodes.Office2_box_6.geometry} material={materials['LP_Rooms.001']} />
             <mesh geometry={nodes.Office2_box_7.geometry} material={materials['LP_Rooms.001']} />
+            <mesh geometry={nodes.Office2_decoration1.geometry} material={materials['LP_Rooms.001']} />
+            <mesh geometry={nodes.Office2_decoration2.geometry} material={materials['LP_Rooms.001']} />
+          
           </group>
         </group>
        
-        <group position= {[0, 0, 0]} rotation={[0, 0, 0]} >
+        <group position= {[0, 0, 0]} rotation={[0, 0, 0]}>
           <group ref = {FurnRef}>
             <mesh geometry={nodes.Office2_little_table.geometry} material={materials['LP_Rooms.001']} />
             <mesh geometry={nodes.Office2_carpet.geometry} material={materials['LP_Rooms.001']} />
-            <mesh geometry={nodes.Office2_chair.geometry} material={materials['LP_Rooms.001']} />
             <mesh geometry={nodes.Office2_clock_1.geometry} material={materials['LP_Rooms.001']} />
-            <mesh geometry={nodes.Office2_decoration1.geometry} material={materials['LP_Rooms.001']} />
-            <mesh geometry={nodes.Office2_decoration2.geometry} material={materials['LP_Rooms.001']} />
             <mesh geometry={nodes.Office2_Flower.geometry} material={materials['LP_Rooms.001']} />
           </group>
         </group>
@@ -206,7 +131,7 @@ export function Office3D(): JSX.Element {
         <group position= {[0, 0, 0]} rotation={[0, 0, 0]} >
           <group ref = {CompRef}>
             <mesh geometry={nodes.Office2_monitor1.geometry} material={materials['LP_Rooms.001']} />
-            <mesh geometry={nodes.Office2_Mouse.geometry} material={materials['LP_Rooms.001']} />
+             <mesh geometry={nodes.Office2_Mouse.geometry} material={materials['LP_Rooms.001']} />
             <mesh geometry={nodes.Office2_Keyboard.geometry} material={materials['LP_Rooms.001']} />
             <mesh geometry={nodes.Office2_Pens.geometry} material={materials['LP_Rooms.001']} />
          </group>
@@ -215,6 +140,7 @@ export function Office3D(): JSX.Element {
         <group position= {[0, 0, 0]} rotation={[0, 0, 0]} >
          <group ref = {RoomRef}>
             <mesh geometry={nodes.Office2_sofa.geometry} material={materials['LP_Rooms.001']} />      
+            <mesh geometry={nodes.Office2_chair.geometry} material={materials['LP_Rooms.001']} />
             <mesh geometry={nodes.Office2_picture.geometry} material={materials['LP_Rooms.001']} />
             <mesh geometry={nodes.Office2_Room_Paper.geometry} material={materials['LP_Rooms.001']} />
             <mesh geometry={nodes.Office2_shelves.geometry} material={materials['LP_Rooms.001']} />
