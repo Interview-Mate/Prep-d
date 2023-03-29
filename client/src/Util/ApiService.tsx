771181bde5478d1618f3a2bbc4e6c9b23157f9a5
Darian Pirowhedayati <d.pirowhedayati@gmail.com>
2023-03-29 21:36:28 +0200

    Update ApiService.tsx

--- client/src/Util/ApiService.tsx
+++ client/src/Util/ApiService.tsx
@@ -1,9 +1,10 @@
 const BASE_URL = process.env.REACT_APP_SERVER_URL;
+
 import axios from "axios";
 
 export const getAllUsers = async () => {
   try {
-    const response = await fetch(`${BASE_URL}/get-all-users`);
+    const response = await fetch(`${process.env.REACT_APP_SERVER_URL}/get-all-users`);
     const users = await response.json();
     return users;
   } catch (error) {
@@ -14,7 +15,7 @@ export const getAllUsers = async () => {
 export const getUser = async (email: string | undefined) => {
   console.log("Email: ", email)
   try {
-    const response = await fetch(`${BASE_URL}/getuser/${email}`);
+    const response = await fetch(`${process.env.REACT_APP_SERVER_URL}/getuser/${email}`);
     const user = await response.json();
     return user;
   } catch (error) {
