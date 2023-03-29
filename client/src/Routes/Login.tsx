dcbb44ab47736c5cb029e23e3bc33c00de17a504
Elise Verhoeye <eliseverhoeye@hotmail.com>
2023-03-29 11:29:57 +0100

    style: login button

--- client/src/Routes/Login.tsx
+++ client/src/Routes/Login.tsx
@@ -22,25 +22,23 @@ export default function Login() {
           <Login3D />
           {!isAuthenticated && (
             <div className="mr-20 flex flex-col justify-center items-center h-full rounded-lg w-1/4 bg-seasalt">
-              <div className="hover:bg-white py-8 border px-8 rounded-lg drop-shadow-lg z-50">
-                <div className="pb-10 underline-offset-8 text-2xl font-bold mt-4 text-eerie-black text-center">
-                  <p> Let's get Prep'd </p>
-                </div>
-                <div className="pb-20 mt-6 text-center text-eerie-black">
-                  <p>
-                    Are you ready to crush your next interview and show those HR
-                    folks who's the boss?
+              <div className="py-10 border px-10 rounded-lg drop-shadow-lg z-50">
+                <div className="pb-10 mt-6 text-center text-eerie-black">
+                  <p className="mb-6">
+                    Are you ready to crush your next interview and show HR who's
+                    the boss?
                   </p>
+                  <h2 className="text-center text-xl font-bold">
+                    Let's get you Prep'd!
+                  </h2>
                 </div>
-                <div className="flex justify-center hover:text-african-violet-400 w-full border-2 border-white bg-african-violet-900 rounded-lg drop-shadow-xl">
+                <div className="flex items-center justify-center">
                   <button
-                    className="font-bold py-2 px-4 rounded mt-4"
+                    type="submit"
+                    className="w-fit py-2 px-4 bg-dark-cyan text-black font-bold text-black hover:bg-african-violet-900 hover:text-seasalt rounded-md px-3 py-2 text-base font-medium"
                     onClick={() => loginWithRedirect()}
                   >
-                    <div className="z-20">
-                      <LockClosedIcon aria-hidden="true" />
-                      Sign in
-                    </div>
+                    Log in/Sign up
                   </button>
                 </div>
               </div>
