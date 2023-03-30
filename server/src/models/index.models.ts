4c9776bb7edecc77396449deea8472a5b01e7ec5
Darian Pirowhedayati <d.pirowhedayati@gmail.com>
2023-03-30 11:26:08 +0200

    Update index.models.ts

--- server/src/models/index.models.ts
+++ server/src/models/index.models.ts
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 import * as dotenv from 'dotenv'
 dotenv.config()
 
-const dbName = process.env.DB_TEST || 'prep-d-test';
+const dbName = process.env.DB_TEST || 'prep-d';
 const dbURL = process.env.DB_URL || 'mongodb://127.0.0.1:27017';
 
 const dbConnection = mongoose.connect(`${dbURL}/${dbName}`, {});
