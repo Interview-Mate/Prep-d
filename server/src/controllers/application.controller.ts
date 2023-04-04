402378edb09dbc0ab8fc703e14e6a9e83df1082e
Darian Pirowhedayati <d.pirowhedayati@gmail.com>
2023-04-04 18:47:42 +0200

    Updated package.json typescript to dependencies

--- server/src/controllers/application.controller.ts
+++ server/src/controllers/application.controller.ts
@@ -3,10 +3,7 @@ config();
 
 import { Request, Response } from "express";
 
-// @ts-ignore
-import pdf from "../libs/pdf-parse/index.js";
-
-// import pdf from "pdf-parse";
+import pdf from "pdf-parse";
 
 import { Configuration, OpenAIApi } from "openai";
 
@@ -48,7 +45,9 @@ const createResume = async (req: Request, res: Response) => {
 
 const getPdfReview = async (req: Request | any, res: Response) => {
   try {
-    let data = await pdf(req.files.file.data)
+    let data = await pdf(req.files.file.data);
+
+
     const response = await openai.createCompletion({
       model: "text-davinci-003",
       prompt: `Review my cover letter: ${data.text}.Rate on a 0-5 scale. Write a text about the quality of the cover letter. Give examples what to improve. The format shoud be: 'Rating: number. Review: review text. Improvement: improvements.'`,
